89c8a72920b6d1ace1833d4bcaf876edc35decdf
StereoLogix <dleduc1@my.centennialcollege.ca>
2024-08-01 14:22:04 -0400

    Configured environment variables for deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 const userRoutes = require('./routes/userRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 const loanRoutes = require('./routes/loanRoutes');
@@ -11,12 +16,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://leesantos:0wqhnTIoKuN3uTw3@comp229-su24.zcztvdz.mongodb.net/?retryWrites=true&w=majority&appName=COMP229-SU24', {    
-}).then(() => {
+// Use environment variable for MongoDB connection string
+const dbURI = process.env.MONGODB_URI;
+mongoose.connect(dbURI, {}).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.error('Failed to connect to MongoDB', err);
 });
+
+// Middleware or any other place where the JWT secret is used
+const jwtSecret = process.env.JWT_SECRET;
 
 app.use('/api/users', userRoutes);
 app.use('/api/expenses', expenseRoutes);
@@ -24,6 +33,7 @@ app.use('/api/loans', loanRoutes);
 app.use('/api/credits', creditRoutes);
 app.use('/api/contact', contactRoutes);
 
+// Use environment variable for PORT
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
